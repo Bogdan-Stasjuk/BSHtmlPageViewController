ffa09d76119d38e18a109b4960bdd208e6440126
Bogdan Stasjuk <Bogdan.Stasjuk@gmail.com>
2014-05-30 10:56:41 +0300

    Initial Commit

---
+++ BSHtmlPageViewControllerUsageTests/BSHtmlPageViewControllerUsageTests.m
@@ -0,0 +1,34 @@
+//
+//  BSHtmlPageViewControllerUsageTests.m
+//  BSHtmlPageViewControllerUsageTests
+//
+//  Created by Bogdan Stasjuk on 5/30/14.
+//  Copyright (c) 2014 Bogdan Stasjuk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BSHtmlPageViewControllerUsageTests : XCTestCase
+
+@end
+
+@implementation BSHtmlPageViewControllerUsageTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
